028f108c61eb43a54d0c548ed686e64f1478108c
0Dark_Phoenix_ <sjoerdpeeneman@outlook.com>
2024-05-27 19:32:36 +0200

    First working version

--- extension.js
+++ extension.js
@@ -1,18 +1,33 @@
-
 const vscode = require('vscode');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    let disposable = vscode.commands.registerCommand('extension.selectPastedText', function () {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
 
+            // Ensure the cursor is in a file, not in the debug console, terminal, etc.
+            if (document.uri.scheme === 'file') {
+                const currentPosition = editor.selection.active;
 
-	context.subscriptions.push(disposable);
+                // Listen for the next paste event
+                vscode.commands.executeCommand('editor.action.clipboardPasteAction').then(() => {
+                    const newPosition = editor.selection.active;
+
+                    // Create a selection from the original cursor position to the new position
+                    const newSelection = new vscode.Selection(currentPosition, newPosition);
+                    editor.selection = newSelection;
+                });
+            }
+        }
+    });
+
+    context.subscriptions.push(disposable);
 }
 
-function deactivate() {}
-
 module.exports = {
-	activate,
-	deactivate
+    activate
 }
