fc5521212d7a5d9bb838c88b4079dd64852902d9
0Dark_Phoenix_ <sjoerdpeeneman@outlook.com>
2024-05-27 19:04:17 +0200

    Initial Commit

---
+++ extension.js
@@ -0,0 +1,18 @@
+
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+
+	context.subscriptions.push(disposable);
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
