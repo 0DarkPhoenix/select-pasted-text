3c08f344b42b2ac694f95ef8669666b3a4944246
0Dark_Phoenix_ <sjoerdpeeneman@outlook.com>
2024-06-08 15:00:40 +0200

    Update v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,22 @@ All notable changes to the "select-pasted-text" extension will be documented in 
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
-## [Unreleased]
+## [1.0.0] - 2024-06-08
 
-- Initial release
+### Added
+
+-   Extra document.uri.scheme properties to support more file types (vscode-userdata, vscode, untitled)
+
+### Changed
+
+-   Slightly altered the color of the logo
+
+## [0.0.2] - 2024-05-27
+
+### Added
+
+-   Logo
+
+## [0.0.1] - YYYY-05-27
+
+### Initial release
--- extension.js
+++ extension.js
@@ -1,33 +1,46 @@
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('extension.selectPastedText', function () {
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const document = editor.document;
+    let disposable = vscode.commands.registerCommand(
+        "extension.selectPastedText",
+        function () {
+            const editor = vscode.window.activeTextEditor;
+            if (editor) {
+                const document = editor.document;
 
-            // Ensure the cursor is in a file, not in the debug console, terminal, etc.
-            if (document.uri.scheme === 'file') {
-                const currentPosition = editor.selection.active;
+                // Ensure the cursor is in a file, not in the debug console, terminal, etc.
+                if (
+                    document.uri.scheme === "file" ||
+                    document.uri.scheme === "vscode-userdata" ||
+                    document.uri.scheme === "vscode" ||
+                    document.uri.scheme === "untitled"
+                ) {
+                    const currentPosition = editor.selection.active;
 
-                // Listen for the next paste event
-                vscode.commands.executeCommand('editor.action.clipboardPasteAction').then(() => {
-                    const newPosition = editor.selection.active;
+                    // Listen for the next paste event
+                    vscode.commands
+                        .executeCommand("editor.action.clipboardPasteAction")
+                        .then(() => {
+                            const newPosition = editor.selection.active;
 
-                    // Create a selection from the original cursor position to the new position
-                    const newSelection = new vscode.Selection(currentPosition, newPosition);
-                    editor.selection = newSelection;
-                });
+                            // Create a selection from the original cursor position to the new position
+                            const newSelection = new vscode.Selection(
+                                currentPosition,
+                                newPosition
+                            );
+                            editor.selection = newSelection;
+                        });
+                }
             }
         }
-    });
+    );
 
     context.subscriptions.push(disposable);
 }
 
 module.exports = {
-    activate
-}
+    activate,
+};
