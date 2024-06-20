ea8eecb8e4c95a038ffdc5fd7755e1b6a3f1de59
Sjoerd Peeneman <sjoerdpeeneman@outlook.com>
2024-06-20 18:43:47 +0200

    Changed code to new formatter suggestions

--- extension.js
+++ extension.js
@@ -4,45 +4,45 @@ const vscode = require("vscode");
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-    let disposable = vscode.commands.registerCommand(
-        "extension.selectPastedText",
-        async function () {
-            const editor = vscode.window.activeTextEditor;
-            if (editor) {
-                const document = editor.document;
+	const disposable = vscode.commands.registerCommand(
+		"extension.selectPastedText",
+		async () => {
+			const editor = vscode.window.activeTextEditor;
+			if (editor) {
+				const document = editor.document;
 
-                // Ensure the cursor is in a file, not in the debug console, terminal, etc.
-                if (
-                    document.uri.scheme === "file" ||
-                    document.uri.scheme === "vscode-userdata" ||
-                    document.uri.scheme === "vscode" ||
-                    document.uri.scheme === "untitled"
-                ) {
-                    // Save the current selection
-                    const currentSelection = editor.selection;
+				// Ensure the cursor is in a file, not in the debug console, terminal, etc.
+				if (
+					document.uri.scheme === "file" ||
+					document.uri.scheme === "vscode-userdata" ||
+					document.uri.scheme === "vscode" ||
+					document.uri.scheme === "untitled"
+				) {
+					// Save the current selection
+					const currentSelection = editor.selection;
 
-                    // Listen for the next paste event
-                    await vscode.commands.executeCommand(
-                        "editor.action.clipboardPasteAction"
-                    );
+					// Listen for the next paste event
+					await vscode.commands.executeCommand(
+						"editor.action.clipboardPasteAction",
+					);
 
-                    // Get the new cursor position after the paste
-                    const newPosition = editor.selection.active;
+					// Get the new cursor position after the paste
+					const newPosition = editor.selection.active;
 
-                    // Create a selection from the start of the original selection to the new position
-                    const newSelection = new vscode.Selection(
-                        currentSelection.start,
-                        newPosition
-                    );
-                    editor.selection = newSelection;
-                }
-            }
-        }
-    );
+					// Create a selection from the start of the original selection to the new position
+					const newSelection = new vscode.Selection(
+						currentSelection.start,
+						newPosition,
+					);
+					editor.selection = newSelection;
+				}
+			}
+		},
+	);
 
-    context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable);
 }
 
 module.exports = {
-    activate,
+	activate,
 };
